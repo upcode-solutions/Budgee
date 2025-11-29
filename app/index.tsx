3f8bd6a3bacb69f9848c3e64cb561c679502ee82
DESKTOP-4TUPSLP\GELO <rezangelodominguez.code@gmail.com>
2025-11-29 11:34:26 +0800

    small steps

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { useThemeContext } from "../context/ThemeContext";
+
+import { ColorScheme } from "../types/ThemeTypes";
 
 export default function Index() {
+  const { colors } = useThemeContext();
+  const design = styles(colors);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={design.container}>
+      <TouchableOpacity>
+        <Text style={{ color: colors.text }}>
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = (colors: ColorScheme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: colors.background,
+    },
+  });
