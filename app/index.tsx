0e0694bdcd1c116565a8cf249c62f0c09e600df2
DESKTOP-4TUPSLP\GELO <rezangelodominguez.code@gmail.com>
2025-11-30 21:48:36 +0800

    PriceModalSample

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,32 @@
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import { useThemeContext } from "../context/ThemeContext";
 
 import { ColorScheme } from "../types/ThemeTypes";
 
+import PriceModal from "../components/modals/PriceModal";
+
 export default function Index() {
   const { colors } = useThemeContext();
   const design = styles(colors);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [value, setValue] = useState(0);
+
   return (
     <View style={design.container}>
-      <TouchableOpacity>
-        <Text style={{ color: colors.text }}>
-          Edit app/index.tsx to edit this screen.
-        </Text>
+      <Text>PHP {value}</Text>
+      <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+        <Text>Show Price Modal</Text>
+      </TouchableOpacity>
+      <PriceModal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        price={value}
+        setPrice={setValue}
+      />
+      <TouchableOpacity onPress={() => setValue(value + 1)}>
       </TouchableOpacity>
     </View>
   );
@@ -26,5 +39,12 @@ const styles = (colors: ColorScheme) =>
       alignItems: "center",
       justifyContent: "center",
       backgroundColor: colors.background,
+    },
+    modelBackground: {
+      padding: 20,
+      borderRadius: 10,
+      backgroundColor: colors.background,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
   });
